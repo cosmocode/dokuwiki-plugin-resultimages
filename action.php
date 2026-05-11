3715a960e8c27f1269a9687cb54adfebe01f9d6f
Anna Dabrowska <dabrowska@cosmocode.de>
2026-05-11 19:53:39 +0200

    Automatic code fixes

--- action.php
+++ action.php
@@ -1,22 +1,25 @@
 <?php
+
+use dokuwiki\Extension\ActionPlugin;
+use dokuwiki\Extension\EventHandler;
+use dokuwiki\Extension\Event;
 /**
  * DokuWiki Plugin resultimages (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Anna Dabrowska <dokuwiki@cosmocode.de>
  */
-
-class action_plugin_resultimages extends DokuWiki_Action_Plugin
+class action_plugin_resultimages extends ActionPlugin
 {
 
     /**
      * Registers a callback function for a given event
      *
-     * @param Doku_Event_Handler $controller DokuWiki's event controller object
+     * @param EventHandler $controller DokuWiki's event controller object
      *
      * @return void
      */
-    public function register(Doku_Event_Handler $controller)
+    public function register(EventHandler $controller)
     {
         $controller->register_hook('SEARCH_RESULT_FULLPAGE', 'BEFORE', $this, 'addImage');
     }
@@ -24,13 +27,13 @@ class action_plugin_resultimages extends DokuWiki_Action_Plugin
     /**
      * Inserts page's first image into the result body
      *
-     * @param Doku_Event $event  event object by reference
+     * @param Event $event event object by reference
      * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      *
      * @return void
      */
-    public function addImage(Doku_Event $event, $param)
+    public function addImage(Event $event, $param)
     {
         // exit if the current result will not even have a preview snippet
         if ($event->data['position'] > FT_SNIPPET_NUMBER) return;
